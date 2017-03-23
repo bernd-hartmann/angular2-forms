ea0fbbf40371f1b23eb4ebca009a702529e8d53a
bernd-hartmann <bernd.hartmann86@t-online.de>
2017-03-23 13:52:00 +0100

    get error keys from angular

--- src/app/business-partner/business-partner.component.ts
+++ src/app/business-partner/business-partner.component.ts
@@ -1,16 +1,50 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, Component, OnInit, ViewChild} from '@angular/core';
 import {BusinessPartner} from "./models/BusinessPartner";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'business-partner',
   templateUrl: 'app/business-partner/business-partner.component.html'
 })
-export class BusinessPartnerComponent {
+export class BusinessPartnerComponent implements OnInit, AfterViewChecked {
+  @ViewChild('bpNgForm') bpNgForm: NgForm;
+
+  public formErrorKeys: Array<string> = new Array();
+  public formElementKeys: Array<string> = ['bpName'];
+
   public bp: BusinessPartner = null;
 
 
   constructor() {
     this.bp = this.createBp();
+
+  }
+
+  public ngOnInit() {
+
+  }
+
+  public ngAfterViewChecked() {
+    this.bpNgForm.valueChanges.subscribe(() => {
+      this.readFormErrors();
+    });
+  }
+
+  public readFormErrors() {
+    // reset
+    this.formErrorKeys = new Array();
+
+    if (!this.bpNgForm) {
+      return;
+    }
+    const form = this.bpNgForm.form;
+
+    this.formElementKeys.forEach((formElementKey: string) => {
+      let formControl = form.get(formElementKey);
+      for (const formErrorKey in formControl.errors) {
+        this.formErrorKeys.push(formElementKey + "." + formErrorKey);
+      }
+    });
   }
 
   private createBp(): BusinessPartner {
