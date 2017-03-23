8430e2ae0f2ed00bcadf4c26ffe9d91d0cc99148
bernd-hartmann <bernd.hartmann86@t-online.de>
2017-03-23 11:45:04 +0100

    basic validation with template driven approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent}  from './app.component';
 import {BusinessPartnerComponent} from './business-partner/business-partner.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, BusinessPartnerComponent],
   bootstrap: [AppComponent]
 })
